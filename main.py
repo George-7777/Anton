aa4bb2517116b8b9e1465998a60d3d24d70a9125
Georgee-7777 <gvayt77@gmail.com>
2023-08-30 21:31:37 +0400

    V3.5 pre-alpha особая версия для преподавателя добавлен

--- main.py
+++ main.py
@@ -14,17 +14,24 @@ morph = pymorphy2.MorphAnalyzer()
 setting = 0
 city = "Ижевск"
 muz = ["1.mp3", "2.mp3", "3.mp3"]
+scazki = ["colobok.mp3", "gusi.mp3", "lyagushka.mp3", "masha.mp3", "medvedi.mp3", "repka.mp3", "terem.mp3"]
 opts = {
     "alias": ('антон', 'антошка', 'антончик', 'антоша', 'антун', 'антоний', 'тоня'),
-    "tbr": ('скажи', 'расскажи', 'покажи', 'сколько', 'произнеси', 'что ты', 'что'),
+    # "tbr": ('скажи', 'расскажи', 'покажи', 'сколько', 'произнеси', 'что ты', 'что'), принят отказ в Антон V 0.4
     "cmds": {
         "ctime": ('текущее время', 'сейчас времени', 'который час'),
         "radio": ('включи музыку', 'воспроизведи радио', 'включи радио'),
         "stupid1": ('расскажи анектод', 'рассмеши меня', 'ты знаешь анектоды'),
         "google": ('найди', "нагугли"),
         "pogoda": ('какая погода в', 'какая погодка в', 'сколько градусов в'),
-        "what": ('такое', 'значит'),
-        "list": ('можешь', 'умеешь')
+        "what": ('что такое', 'что значит'),
+        "list": ('что ты можешь', 'что ты умеешь'),
+        "scazki": ('расскажи сказку', 'включи сказку', 'хочу сказку'),
+        "youtube": ('включи ютуб', 'открой ютуб'),
+        "gog": ('включи гугл', 'открой гугл', 'включи google', 'открой google'),
+        "ya": ('включи яндекс', 'открой яндекс'),
+        "uchi": ('включи учи ру', 'открой учи ру', 'включи uchi.ru', 'открой uchi.ru', 'uchi.ru'),
+        "figna": ('включи skysmart', 'открой skysmart', 'включи скайсмарт', 'открой скайсмарт'),
 
     }
 }
@@ -38,10 +45,10 @@ def speak(what):
     speak_engine.stop()
 
 
-def callback(recognizer, audio):
+def callback(audio):
     print("работаем-работаем")
     try:
-        voice = recognizer.recognize_google(audio, language="ru-RU").lower()
+        voice = r.recognize_google(audio, language="ru-RU").lower()
         print("[log] Распознано: " + voice)
 
         if voice.startswith(opts["alias"]) or setting == 0:  # if voice.startswith(opts["alias"] or setting == 0):
@@ -52,8 +59,8 @@ def callback(recognizer, audio):
             for x in opts['alias']:
                 cmd = cmd.replace(x, "").strip()
 
-            for x in opts['tbr']:
-                cmd = cmd.replace(x, "").strip()
+            #for x in opts['tbr']:
+                #cmd = cmd.replace(x, "").strip() принят отказ в Антон V0.4
             global sapros
 
             sapros = cmd
@@ -61,21 +68,17 @@ def callback(recognizer, audio):
                 sapros = sapros.replace(x, "").strip()
             # распознаем и выполняем команду
 
-            cmd = cmd.split()
-            if len(cmd) > 1:
-                cmd = cmd[0]
 
-            cmd = "".join(cmd)
 
             cmd = recognize_cmd(cmd)
 
             execute_cmd(cmd['cmd'])
-    except Exception as e:
-        print(e)
+    except sr.RequestError:
+        print("[log] Неизвестная ошибка, проверьте интернет!")
     except sr.UnknownValueError:
         print("[log] Голос не распознан!")
-    except sr.RequestError as e:
-        print("[log] Неизвестная ошибка, проверьте интернет!")
+    except Exception as e:
+        print(e)
 
 
 
@@ -96,7 +99,7 @@ def recognize_cmd(cmd):
 def execute_cmd(cmd):
     gcmd = sapros.split()
 
-    #gcmd = gcmd[1:]
+    # gcmd = gcmd[1:]
     print(gcmd)
     if cmd == 'ctime':
         # сказать текущее время
@@ -104,9 +107,9 @@ def execute_cmd(cmd):
         speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
 
     elif cmd == 'radio':
-        #музончик)
+        # музончик)
         os.system(random.choice(muz))
-        #os.system("C:\\Users\\User\\PycharmProjects\\Anton\\res\\mus.mp3")
+        # os.system("C:\\Users\\User\\PycharmProjects\\Anton\\res\\mus.mp3")
 
     elif cmd == 'stupid1':
         # рассказать анекдот
@@ -118,7 +121,7 @@ def execute_cmd(cmd):
 
         city = gcmd[-1]
         city = morph.parse(city)[0].normal_form
-        url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+        url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
         weather_data = requests.get(url).json()
         # получаем данные о температуре и о том, как она ощущается
         temperature = round(weather_data['main']['temp'])
@@ -139,7 +142,19 @@ def execute_cmd(cmd):
         ''')
     elif cmd == 'what':
         speak(wikipedia.summary(" ".join(gcmd[1:]), sentences=3))
+    elif cmd == 'scazki':
 
+        os.system(f"сказки\\{random.choice(scazki)}")
+    elif cmd == 'youtube':
+        wb.open("https://www.youtube.com/")
+    elif cmd == 'gog':
+        wb.open("https://www.google.ru/")
+    elif cmd == 'figna':
+        wb.open("https://skysmart.ru/")
+    elif cmd == 'ya':
+        wb.open("https://ya.ru/")
+    elif cmd == 'uchi':
+        wb.open("https://uchi.ru/")
     else:
         speak("Команда не распознана")
 
@@ -158,8 +173,9 @@ speak_engine = pyttsx3.init()
 
 speak("Приветствую, я Антон")
 speak("Чем я могу помочь?")
-stop_listening = r.listen_in_background(m, callback)
+#stop_listening = r.listen_in_background(m, callback)
+
+aud = r.listen(m)
+callback(aud)
 
 while True: time.sleep(0.1)  # infinity loop
-
-
